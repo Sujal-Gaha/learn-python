37c0e2cc7044c93d4c660f026da355c1a47fd4e8
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-03-15 09:34:13 +0545

    made instances of balls using loops for experimentation purpose

--- pygame_projects/bouncing_ball.py
+++ pygame_projects/bouncing_ball.py
@@ -1,14 +1,16 @@
 import pygame
+import random
 
 # Constants
 HEIGHT, WIDTH = 700, 1000
-BLUE = "#0000FF"
+RED,GREEN, BLUE = "#FF0000", "#00FF00", "#0000FF"
 WHITE = "#FFFFFF"
 BALL_RADIUS = 30
 BALL_INITIAL_VELOCITY = 0
 GRAVITY = 0.3
 BOUNCE_DAMPING = 0.8 # Set to 0.8 means that the ball conserves 80% of its energy
 FPS = 60
+BALL_COUNT = 20
 
 pygame.init()
 
@@ -23,6 +25,7 @@ class Ball:
         self.velocity = BALL_INITIAL_VELOCITY
         self.acceleration = GRAVITY
         self.radius = BALL_RADIUS
+        self.color = random.choice([RED, GREEN, BLUE])
 
     def update(self):
         self.y += self.velocity
@@ -40,8 +43,13 @@ class Ball:
     def draw(self):
         pygame.draw.circle(screen, BLUE, (self.x, self.y), self.radius, 0)
         
-ball_one = Ball((WIDTH - (0.2 * WIDTH)) // 2, HEIGHT //2)
-ball_two = Ball((WIDTH + (0.2 * WIDTH)) // 2, HEIGHT //2)
+balls = [
+     Ball(
+        x=random.randint(100, WIDTH - 100),
+        y=random.randint(100, HEIGHT // 2)
+    ) 
+    for _ in range(BALL_COUNT)
+]
 
 running = True
 while running:
@@ -51,11 +59,9 @@ while running:
 
     screen.fill(WHITE)
 
-    ball_one.update()
-    ball_two.update()
-
-    ball_one.draw()
-    ball_two.draw()
+    for ball in balls:
+        ball.update()
+        ball.draw()
 
     pygame.display.update()
     clock.tick(FPS)
