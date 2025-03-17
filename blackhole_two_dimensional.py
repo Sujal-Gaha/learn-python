2fad756882a376f26d3cf16f853d82ec8748396a
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-03-17 13:18:08 +0545

    added a way for creating the blackhole instance

--- blackhole_two_dimensional.py
+++ blackhole_two_dimensional.py
@@ -1,22 +1,25 @@
 import pygame
 import numpy as np
 
-
 WIDTH, HEIGHT = 800, 800
 
-# Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
 
-# Black Hole properties
-bh_x, bh_y = WIDTH // 2, HEIGHT // 2
-bh_radius = 30
-
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+
+class BlackHole:
+    def __init__(self, bh_x, bh_y, bh_radius):
+        self.x = bh_x;
+        self.y = bh_y;
+        self.radius = bh_radius;
+
+    def draw(self):
+        pygame.draw.circle(screen, BLACK, (self.x, self.y), self.radius)
 
 class CelestialBody:
     def __init__(self, color, orbit_radius, speed, size):
@@ -31,19 +34,21 @@ class CelestialBody:
         self.y = int(center_y + self.orbit_radius * np.sin(self.angle))
         self.angle += self.speed
 
-    def draw(self, surface):
-        pygame.draw.circle(surface, self.color, (self.x, self.y), self.size)
+    def draw(self):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
+
+blackhole = BlackHole(WIDTH // 2, HEIGHT // 2, 20)
 
 planet = CelestialBody(YELLOW, orbit_radius=200, speed=0.02, size=10)
 
 running = True
 while running:
-    screen.fill(BLACK)
-    
-    pygame.draw.circle(screen, WHITE, (bh_x, bh_y), bh_radius)
-    
-    planet.update_position(bh_x, bh_y)
-    planet.draw(screen)
+    screen.fill(WHITE)
+
+    blackhole.draw()
+        
+    planet.update_position(blackhole.x, blackhole.y)
+    planet.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
